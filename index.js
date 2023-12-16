eee624aed5717a14bfe1197d6c1f5aa6a856052a
Javier0003 <jpere0500@gmail.com>
2023-12-16 16:19:15 -0400

    added storing the folders to next and preview not loosing the folder when closing the app styles redesigned

--- index.js
+++ index.js
@@ -1,29 +1,99 @@
+let list, order
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.bookmarks.getTree(function (bookmarks) {
-    displayBookmarks(bookmarks[0].children)
-  })
+  list = JSON.parse(localStorage.getItem('bookmarks'))
+  order = localStorage.getItem('order')
+  localStorage.removeItem('order')
+  if(!order){
+    order = -1
+  }
+  if(!list){
+    chrome.bookmarks.getTree(function (bookmarks) {
+      displayBookmarks(bookmarks[0].children)
+    })
+  }
 })
 const bookmarksList = document.getElementById('bookmarksList')
-
+const btns = {
+  forward: document
+    .getElementById('forward')
+    .addEventListener('click', function () {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function (tabs) {
+          if(list){
+            order++
+            if(order >= list.length - 1){
+              order = list.length - 1
+              return 
+            }
+            localStorage.removeItem('order')
+            localStorage.setItem('order', order)
+            chrome.tabs.update(tabs[0].id, { url: list[order] })
+          }
+        }
+      )
+    }),
+  backward: document
+    .getElementById('backward')
+    .addEventListener('click', function () {
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function (tabs) {
+          if(list){
+            order--
+            if(order <= -1) {
+              order = 0
+              return
+            }
+            localStorage.removeItem('order')
+            localStorage.setItem('order', order)
+            chrome.tabs.update(tabs[0].id, { url: list[order] })
+          }
+        }
+      )
+    }),
+  start: document
+    .getElementById('start')
+    .addEventListener('click', function () {
+      localStorage.setItem('bookmarks', JSON.stringify(playlist))
+      chrome.runtime.reload();
+    }),
+  stop: document
+    .getElementById('stop')
+    .addEventListener('click', function () {
+      localStorage.clear()
+      chrome.runtime.reload();
+    }),
+}
+let playlist = []
 function displayBookmarks(bookmarks) {
   bookmarks.forEach(function (bookmark) {
-    const li = document.createElement('li')
-    const a = document.createElement('a')
-    a.href = bookmark.url || '#'
-    a.textContent = bookmark.title
-    a.id = `${bookmark.id}_a`
-    li.class = `list`
-    li.addEventListener('click', function () {
-      const listEl = document.querySelectorAll(`li`)
-      if(bookmark.children){
+    const button = document.createElement('button')
+    button.innerHTML = bookmark.title
+    if (!bookmark.children) {
+      playlist.push(bookmark.url)
+      button.className = `link`
+      button.addEventListener('click', function () {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.update(tabs[0].id, { url: bookmark.url || '#' })
+          }
+        )
+      })
+    }
+    if (bookmark.children) {
+      button.className = `folder`
+      button.addEventListener('click', function () {
+        order = -1
+        const listEl = document.querySelectorAll(`button`)
         listEl.forEach((element) => {
           element.remove()
         })
-      }
-      const passedMarks = bookmark.children
-      displayBookmarks(passedMarks)
-    })
-    li.appendChild(a)
-    bookmarksList.appendChild(li)
+        const passedMarks = bookmark.children
+        displayBookmarks(passedMarks)
+      })
+    }
+    bookmarksList.appendChild(button)
   })
 }
